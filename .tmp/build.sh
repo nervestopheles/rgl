6e0546745cf32d3349054e06890150b3fdeb08e7
Soma Nervestopheles <nervestopheles@gmail.com>
2022-08-19 02:31:20 +0700

    add compressed build shell script

---
+++ .tmp/build.sh
@@ -0,0 +1,12 @@
+!#/bin/bash
+set -e
+
+NAME=void
+
+export LD_LIBRARY_PATH+=:$(rustc --print=sysroot)/lib
+export RUSTFLAGS="-C prefer-dynamic"
+
+cargo build --release \
+&& upx --best --lzma ./target/release/${NAME} \
+&& echo "Compressed build complete." \
+&& ls -lh --color=auto ./target/release/${NAME}
